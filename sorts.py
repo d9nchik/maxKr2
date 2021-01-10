d4035a151bfedb8f4cd787420a9dcc5e4f4c291f
d9nich <dan.galaiko@gmail.com>
2021-01-10 23:56:37 +0200

    added natural merge

--- sorts.py
+++ sorts.py
@@ -52,5 +52,46 @@ def direct_merge(array, k=0):
     return direct_merge(c, k + 1)
 
 
+def natural_merge(array):
+    arrays = [[], []]
+    previous_value = float('-inf')
+    for element in array:
+        if previous_value > element: arrays.reverse()
+        arrays[0].append(element)
+        previous_value = element
+
+    if len(arrays[1]) == 0:
+        print('end')
+        return array
+
+    a, b = arrays
+    print('a = {}'.format(a))
+    print('b = {}'.format(b))
+    c = []
+
+    while len(a) > 0 and len(b) > 0:
+        previous_value_a = float('-inf')
+        previous_value_b = float('-inf')
+        while len(a) > 0 and len(b) > 0 and previous_value_a < a[0] and previous_value_b < b[0]:
+            if a[0] < b[0]:
+                previous_value_a = a.pop(0)
+                c.append(previous_value_a)
+            else:
+                previous_value_b = b.pop(0)
+                c.append(previous_value_b)
+
+        while len(a) > 0 and previous_value_a < a[0]:
+            previous_value_a = a.pop(0)
+            c.append(previous_value_a)
+
+        while len(b) > 0 and previous_value_b < b[0]:
+            previous_value_b = b.pop(0)
+            c.append(previous_value_b)
+
+    print('array after merge = {}'.format(c))
+    return natural_merge(c)
+
+
 if __name__ == '__main__':
-    print(direct_merge(array))
+    # print(direct_merge(array))
+    print(natural_merge(array))
