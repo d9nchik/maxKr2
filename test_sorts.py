0c81756889b04327d57e9dd0eefd75c1c74a19e3
d9nich <dan.galaiko@gmail.com>
2021-01-11 11:25:44 +0200

    Add simple test for CI

---
+++ test_sorts.py
@@ -0,0 +1,20 @@
+from unittest import TestCase
+
+
+class Test(TestCase):
+    def test(self):
+        self.assertTrue(1 == 1)
+#     def test_direct_merge(self):
+#         self.fail()
+#
+#     def test_natural_merge(self):
+#         self.fail()
+#
+#     def test_balanced_poly_path_merge(self):
+#         self.fail()
+#
+#     def test_fibonacci_n(self):
+#         self.fail()
+#
+#     def test_print_ideal_division(self):
+#         self.fail()
